655403b32a16366098347845e10fe1bc53d0123c
ArisGoi <aris.goi97@gmail.com>
2021-08-30 16:45:24 +0200

    add stampa risultati

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ btnCrea.addEventListener('click',
         var getName = document.getElementById('name').value;
         var getEta = document.getElementById('eta').value;
         var getDate = document.getElementById('dateForm').value;
-        var getFrom = "MI";
+        var getFrom = "Milano";
         var getDestination = document.getElementById('destination').value;
         var getClass = document.getElementById('classe').value;
 
@@ -20,9 +20,9 @@ btnCrea.addEventListener('click',
                 var ticketCode = parseInt(Math.random() * 100000);
                 // 2b) Generazione codice posto a sedere
                 var randomSeat = parseInt(Math.random() * 30 + 1);
-                if (getClass == "first") {
+                if (getClass == "Prima") {
                     var seatNumber = "01" + " - " + randomSeat;
-                } else if (getClass == "eco"){
+                } else if (getClass == "Economy"){
                     var randomVagon = parseInt(Math.random() * 15 + 1);
                     // Il while impedisce di generare una carrozza 0 oppure 1(prima classe)
                     while (randomVagon == 1 || randomVagon == 0){
@@ -30,9 +30,9 @@ btnCrea.addEventListener('click',
                     }
                     // impedisce una sola cifra nel numero vagone
                     if (randomVagon < 10){
-                        seatNumber = "0" + randomVagon;
+                        seatNumber = "0" + randomVagon + " - " + randomSeat;
                     } else {
-                        seatNumber = randomVagon;
+                        seatNumber = randomVagon + " - " + randomSeat;
                     }
                 };
 
@@ -42,25 +42,72 @@ btnCrea.addEventListener('click',
                 // 4) Calcolo lo sconto
                 var sconto;
 
-                if (getEta == "minor"){
+                if (getEta == "Minorenne"){
                     sconto = (ticketPrice * 20 /100);
-                } else if (getEta == "overEta"){
+                } else if (getEta == "Over65"){
                     sconto = (ticketPrice * 40 /100);
                 } else {
                     sconto = 0;
                 }
 
                 // 5) Calcolo il prezzo finale
-                var finalPrice = ticketPrice - sconto;
-                finalPrice = (Math.round(finalPrice * 100) / 100).toFixed(2);
+                if (getClass == "Prima") { // Se il posto è in prima classe il prezzo raddoppia
+                    var finalPrice = ticketPrice - sconto;
+                    finalPrice = (Math.round(finalPrice * 100) / 100 * 2).toFixed(2);
+                } else {
+                    var finalPrice = ticketPrice - sconto;
+                    finalPrice = (Math.round(finalPrice * 100) / 100).toFixed(2);
+                }
 
 
-                // FINE
+                // 6) Stampo i risultati
                 console.log(getName, getEta, getDate, getDestination, getClass);
                 console.log(ticketCode);
                 console.log(seatNumber);
-
                 console.log(finalPrice);
+
+                    // nome e cognome
+                    document.getElementById('innName').innerHTML = getName;
+                    // Categoria passeggero (ETA)
+                    document.getElementById('innEta').innerHTML = getEta;
+                    // Data Biglietto
+                    document.getElementById('innDate').innerHTML = getDate;
+                    // Partenza
+                    document.getElementById('innFrom').innerHTML = getFrom;
+                    // Destinazione
+                    if (getDestination == 216){
+                        // Bologna
+                        document.getElementById('innDestination').innerHTML = "Bologna";
+                    } else if (getDestination == 306){
+                        // Firenze
+                        document.getElementById('innDestination').innerHTML = "Firenze";
+                    } else if (getDestination == 764){
+                        // Napoli
+                        document.getElementById('innDestination').innerHTML = "Napoli";
+                    } else if (getDestination == 581){
+                        // Pescara
+                        document.getElementById('innDestination').innerHTML = "Pescara";
+                    } else if (getDestination == 575){
+                        // Roma
+                        document.getElementById('innDestination').innerHTML = "Roma";
+                    } else if (getDestination == 966){
+                        // Taranto
+                        document.getElementById('innDestination').innerHTML = "Taranto";
+                    } else if (getDestination == 270){
+                        // Venezia
+                        document.getElementById('innDestination').innerHTML = "Venezia";
+                    } else if (getDestination == 97){
+                        // Verbania
+                        document.getElementById('innDestination').innerHTML = "Verbania";
+                    };
+                    // Classe
+                    document.getElementById('innClass').innerHTML = getClass;
+                    // Posto a sedere
+                    document.getElementById('innSeat').innerHTML = seatNumber;
+                    // Codice biglietto
+                    document.getElementById('innCode').innerHTML = ticketCode;
+                    // Prezzo Biglietto
+                    document.getElementById('innPrice').innerHTML = finalPrice + " €";
             }
 
     }
